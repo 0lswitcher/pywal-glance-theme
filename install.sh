812de424ecb0b0e6ab66b7427b643268ed64fbec
0lswitcher <manjarodistro@gmail.com>
2026-06-20 16:57:52 -0500

    Fix links to source script

--- install.sh
+++ install.sh
@@ -2,13 +2,13 @@
 # install.sh - installs pywal-to-glance.sh from bash-scripts repo
 
 echo "Installing pywal-to-glance.sh..."
-curl -sLO https://raw.githubusercontent.com/0lswitcher/bash-scripts/refs/heads/main/scripts/pywal-glance-theme.sh
+curl -sLO https://raw.githubusercontent.com/0lswitcher/bash-scripts/refs/heads/main/scripts/pywal-to-glance.sh
 
 echo "Updating execution permissions..."
-chmod +x ./pywal-glance-theme.sh
+chmod +x ./pywal-to-glance.sh
 
 echo "Moving script to ~/.local/bin..."
 echo "Feel free to relocate!"
-mkdir -p ~/.local/bin && mv ./pywal-glance-theme.sh ~/.local/bin/
+mkdir -p ~/.local/bin && mv ./pywal-to-glance.sh ~/.local/bin/
 
 echo "Installation complete!"
